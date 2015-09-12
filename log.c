1c6f1cdeac8c26b35a0b7f9d07d0bfac48a67e9b
feixiaoxing <feixiaoxing@163.com>
2015-09-13 05:51:39 +0800

    fulfill log.c file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- log.c
+++ log.c
@@ -2,19 +2,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "type.h"
 #include "log.h"
 
-void log_print_error() {
-	
-	
+static u32 debug_level;
+
+// init debug
+
+void init_debug() {
+
+	debug_level = 0;
 }
 
-void log_print_warn() {
-	
-	
+// set debug level
+
+void set_debug_level(u32 val) {
+
+	debug_level = val;
 }
 
-void log_print_msg() {
+// only print error message
+
+void log_print_error(const char* fmt, ...) {
+
+	va_list  args;
+	char buffer[1024];
+
+    if (debug_level >= LOG_ERROR_LEVEL) {
 	
+        va_start(args, fmt);
+        vsprintf(buffer, fmt, args);
+        va_end(args);
 	
+		printf("%s", buffer);
+    }
+
+}
+
+// print only warn message
+
+void log_print_warn(const char* fmt, ...) {
+
+	va_list  args;
+	char buffer[1024];
+
+    if (debug_level >= LOG_WARN_LEVEL) {
+	
+        va_start(args, fmt);
+        vsprintf(buffer, fmt, args);
+        va_end(args);
+	
+		printf("%s", buffer);
+    }
+
+}
+
+// print normal log message
+
+void log_print_msg(const char* fmt) {
+
+	va_list  args;
+	char buffer[1024];
+
+    if (debug_level >= LOG_MSG_LEVEL) {
+	
+        va_start(args, fmt);
+        vsprintf(buffer, fmt, args);
+        va_end(args);
+	
+		printf("%s", buffer);
+    }
 }
