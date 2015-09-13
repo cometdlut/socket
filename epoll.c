22e6f275e20f70f88aebaab3529d3e0368acab2b
feixiaoxing <feixiaoxing@163.com>
2015-09-13 10:52:35 +0800

    fulfill epoll.c file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- epoll.c
+++ epoll.c
@@ -2,21 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int epoll;
+
 void epoll_init() {
 	
+	epoll = epoll_create(5);
 }
 
-void epoll_add_socket() {
+void epoll_add_socket(int socket) {
 	
-	
+	epoll_ctl();
 }
 
-void epoll_del_socket() {
+void epoll_del_socket(int socket) {
 	
-	
+	epoll_ctl();
 }
 
 void epoll_run() {
 	
+	while(1) {
+
+		epoll_wait();
+
+	}
 	
 }
