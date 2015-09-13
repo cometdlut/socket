9bcdd89cccad2f9c87614c13caea7b00248de1f3
feixiaoxing <feixiaoxing@163.com>
2015-09-13 10:58:41 +0800

    fulfill select.c file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- select.c
+++ select.c
@@ -7,15 +7,19 @@ void select_init() {
 }
 
 void select_add_socket() {
-	
-	
+
+	FD_SET();
 }
 
 void select_del_socket() {
-		
+
+	FD_SET();
 }
 
 void select_run() {
 	
-	
+	while(1) {
+
+		select();
+	}
 }
