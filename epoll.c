ca28628e70fc216ad87332eb245b7543a803cdff
feixiaoxing <feixiaoxing@163.com>
2015-09-14 18:03:34 +0800

    fix bug of epoll.c file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- epoll.c
+++ epoll.c
@@ -6,24 +6,20 @@ static int epoll;
 
 void epoll_init() {
 	
-	epoll = epoll_create(5);
 }
 
 void epoll_add_socket(int socket) {
 	
-	epoll_ctl();
 }
 
 void epoll_del_socket(int socket) {
 	
-	epoll_ctl();
 }
 
 void epoll_run() {
 	
 	while(1) {
 
-		epoll_wait();
 
 	}
 	
