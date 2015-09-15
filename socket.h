378504e1557b0aa79b08b090080c8cbfb0cc5313
feixiaoxing <feixiaoxing@163.com>
2015-09-15 16:41:37 +0800

    fulfill demo.c file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- socket.h
+++ socket.h
@@ -2,9 +2,8 @@
 #ifndef _SOCKET_H
 #define _SOCKET_H
 
-void create_socket();
-void listen_socket();
-void bind_socket();
-void connect_socket();
+STATUS create_socket(int *sock);
+STATUS bind_socket(int sock, u32 ip, u16 port);
+STATUS listen_socket(int sock);
 
 #endif
