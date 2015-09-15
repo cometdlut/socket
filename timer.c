637ded798262e9cb8e096875c04ea74a89b1fef9
feixiaoxing <feixiaoxing@163.com>
2015-09-15 10:51:13 +0800

    fulfill timer.c file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- timer.c
+++ timer.c
@@ -1,2 +1,79 @@
 
+#include "type.h"
 #include "timer.h"
+#include "log.h"
+
+static int g_tick;
+static ListNode g_timer_head;
+
+// init timer module
+
+void timer_init() {
+
+	g_tick = 0;
+	init_node(&g_timer_head);
+}
+
+// register timer module
+
+void timer_add(int gap, void(*func)(void*), void* param) {
+
+	timer tm;
+	timer* p_tm;
+	ListNode* p_node;
+
+	init_node(&tm.node);
+	tm.tick = g_tick + gap;
+	tm.func = func;
+	tm.param = param;
+
+	p_node = g_timer_head.next;
+	for(; p_node != &g_timer_head; p_node = p_node->next) {
+
+		p_tm = (timer*) (p_node);
+		if(p_tm-> tick > tm.tick){
+			break;
+		}
+	}
+
+	add_node(p_node, &tm.node);
+}
+
+// delete timer module
+
+void timer_delete(timer* p_tm) {
+
+	delete_node(&p_tm->node);
+}
+
+// update timer by period function
+
+void update_timer() {
+
+	timer* p_tm;
+	ListNode* p_prev;
+	ListNode* p_node;
+
+	g_tick ++;
+	p_node = g_timer_head.next;
+
+	while(1){
+
+		if(p_node == &g_timer_head){
+			break;
+		}
+
+		p_tm = (timer*)(p_node);
+		if(p_tm->tick > g_tick) {
+			break;
+		}
+
+		p_prev = p_node;
+		p_node = p_node-> next;
+
+		p_tm->func(p_tm-> param);
+
+	}
+
+}
+
--- timer.h
+++ timer.h
@@ -2,4 +2,25 @@
 #ifndef _TIMER_H
 #define _TIMER_H
 
+#include "list.h"
+
+// define struct
+
+typedef struct _timer {
+
+	ListNode node;
+	u32 tick;
+	void(*func)(void*);
+	void* param;
+
+}timer;
+
+// define function
+
+
+void timer_init();
+void timer_add(int gap, void(*func)(void*), void* param);
+void timer_del(timer* p_tm);
+void update_timer();
+
 #endif
