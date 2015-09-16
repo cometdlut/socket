08fb4c3d1ba5d27b61561c33fc28ba59e22e1f4c
feixiaoxing <feixiaoxing@163.com>
2015-09-16 09:17:45 +0800

    fulfill pipe.c file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- pipe.c
+++ pipe.c
@@ -1,2 +1,44 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "type.h"
 #include "pipe.h"
+#include "log.h"
+
+static int pipe_ch[2];
+
+// init pipe module
+
+void init_pipe() {
+
+	if(pipe(pipe_ch) < 0) {
+
+		log_print_error("Failed to create pipe!\n");
+	}
+}
+
+// write pipe
+
+void write_pipe(s8* buf, u32 len) {
+
+	write(pipe_ch[0], buf, len);
+}
+
+// read pipe
+
+void read_pipe(s8* buf, u32 len) {
+
+	read(pipe_ch[1], buf, len);
+}
+
+// close pipe
+
+void close_pipe() {
+
+	close(pipe_ch[0]);
+	close(pipe_ch[1]);
+}
+
