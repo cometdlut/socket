dde9d24b4ddfd1d2bd5aa16efe4a4e59e50acc35
feixiaoxing <feixiaoxing@163.com>
2015-09-17 08:26:47 +0800

    fulfill config.c file
    Signed-off-by: feixiaoxing <feixiaoxing@163.com>

--- config.c
+++ config.c
@@ -1,2 +1,51 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "type.h"
 #include "config.h"
+#include "log.h"
+
+static u32 ip;
+static u32 port;
+
+void load_config() {
+
+	s8 buf[1024];
+	u32 len;
+
+	// read file first
+
+	FILE* stream;
+	stream = fopen("base.config", "rw+");
+	if(stream) {
+
+		log_print_error("Failed to open config file. \n");
+		return;
+	}
+
+	len = fread(buf, 1024, 1, stream);
+	if(!len) {
+
+		log_print_error("Failed to read config file. \n");
+		return;
+	}
+
+	fclose(stream);
+
+	// then parse data
+
+	sscanf(buf, "%d:%d", &ip, &port);
+}
+
+u32 get_parse_ip() {
+
+	return ip;
+}
+
+u16 get_parse_port() {
+
+	return (u16)port;
+}
+
